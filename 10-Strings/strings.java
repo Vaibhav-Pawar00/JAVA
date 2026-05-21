1a4f412aa0b6dee48eccd00d5c6e7b7886475a76
Vaibhav-Pawar00 <vjpawr05@gmail.com>
2026-05-21 12:16:32 +0530

    largest string based on lexicographic

--- 10-Strings/strings.java
+++ 10-Strings/strings.java
@@ -7,6 +7,24 @@ public class strings {
         }
     }
 
+    public static String printSubstring(String str, int si, int ei){
+        String substring = "";
+        for(int i=si; i<ei; i++){
+            substring += str.charAt(i);
+        }
+        return substring;
+    }
+
+    public static void largestString(String str[]){
+        String largest = str[0];
+        for(int i=1; i<str.length; i++){
+            if(largest.compareToIgnoreCase(str[i]) < 0){
+                largest = str[i];
+            }
+        }
+        System.out.println("The largest string (based on lexicographic) is: " + largest);
+    }
+
     public static void main(String args[]){
         /*
         String str = "abcd";
@@ -16,20 +34,22 @@ public class strings {
         String text = sc.nextLine();
         System.out.println(text);
         */
+/*
+String name = "Tony Stark";
+System.out.println(name.length());
+System.out.println(name.charAt(0));
 
-        String name = "Tony Stark";
-        System.out.println(name.length());
-        System.out.println(name.charAt(0));
-
-        // concatenation
-        String fName = "Vaibhav";
-        String lName = "Pawar";
-        String fullName = fName + " " + lName;
-        // System.out.println(fullName);
-        printString(fullName);
+// concatenation
+String fName = "Vaibhav";
+String lName = "Pawar";
+String fullName = fName + " " + lName;
+// System.out.println(fullName);
+printString(fullName);
+*/
 
 
         // String Comparison
+    /*
         String s1 = "Tony";
         String s2 = "Tony";
         String s3 = new String("Tony");
@@ -39,17 +59,29 @@ public class strings {
         }else{
             System.out.println("Strings are not equal!!");
         }    
-        /*
         if (s1 == s3) {   // -----------> returns false since checks object as new object is created 
         System.out.println("Strings are equal");
         }else{
         System.out.println("Strings are not equal!!");
-        }*/
-        if(s1.equals(s3)){ // -----------> returns true checks since used - .equal function and compared inside value not object.
-            System.out.println("Strings are equal ");
-        }else{
-            System.out.println("Not equal");
         }
+        if(s1.equals(s3)){ // -----------> returns true checks since used - .equal function and compared inside value not object.
+        System.out.println("Strings are equal ");
+        }else{
+        System.out.println("Not equal");
+        }
+    */
+
+
+        // SubString 
+        // String str = "HelloWorld";
+        // System.out.println(printSubstring(str, 0, 5));
+        // Inbuild Substring method --->
+        // System.out.println(str.substring(0,5));
+
+
+        // Largest String -----> based on lexicographic, not length of the string
+        String fruits[] = {"apple", "mango", "banana"};
+        largestString(fruits);
 
         // sc.close();
 
