714de026ab41ccc059003849e083dc427038ddcd
Vaibhav-Pawar00 <vjpawr05@gmail.com>
2026-05-21 13:18:56 +0530

    Convert 1st letter to Upper Case

--- 10-Strings/stringBuilder.java
+++ 10-Strings/stringBuilder.java
@@ -1,9 +1,36 @@
 public class stringBuilder {
+
+    public static String toUpperCase(String str){
+        StringBuilder sb = new StringBuilder("");
+
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for(int i=1; i<str.length(); i++){
+            if(str.charAt(i) == ' ' && i<str.length()-1){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }else{
+                sb.append(str.charAt(i));
+            }
+        }
+
+        return sb.toString();
+        
+    }
+
     public static void main(String args[]){
+
+        /*
         StringBuilder sb = new StringBuilder("");
         for(char ch='a'; ch<='z'; ch++){
             sb.append(ch);
         }
         System.out.println(sb);
+        */
+
+        String str = "hi, i am vaibhav pawar";
+        System.out.println(toUpperCase(str));
     }
 }
