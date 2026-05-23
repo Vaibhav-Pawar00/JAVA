da0aacf8d0f0a769af3bd1b23f6138cc6b272dd2
Vaibhav-Pawar00 <vjpawr05@gmail.com>
2026-05-24 05:09:37 +0530

    Get, Set, Clear & Update ith Bit

--- 11 - Bit Manipulation/bitQues.java
+++ 11 - Bit Manipulation/bitQues.java
@@ -9,10 +9,56 @@ public class bitQues {
         }
     }
 
+    public static int getIthBit(int n, int i){
+        int bitMask = 1<<i;
+        if((n&bitMask) == 0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+
+    public static int setIthBit(int n, int i){
+        int bitMask = 1<<i;
+        return n | bitMask;
+    }
+
+    public static int clearIthBit(int n, int i){
+        int bitMask = ~(1<<i);
+        return n & bitMask;
+    }
+
+    public static int updateIthBit(int n, int i, int newBit){
+        // Method - 1
+        // if(newBit == 0){
+        //     return clearIthBit(n, i);
+        // }else{
+        //     return setIthBit(n, i);
+        // }
+
+        // Method - 2
+        n = clearIthBit(n, i);
+        int bitMask = newBit << i;
+        return n | bitMask;
+    }
+
     public static void main(String []args){
+        // Odd or Even using Bit Manioulation
         oddOrEven(3);
         oddOrEven(17);
         oddOrEven(26);
+
+        // get ith Bit - get the what is there in the ith postion 0 or 1
+        System.out.println(getIthBit(10, 2));
+
+        // set ith bit - set or change the ith bit to 0 if 1 or 1 if 0
+        System.out.println(setIthBit(10, 2));
+
+        // clear ith bit - clears to 0
+        System.out.println(clearIthBit(10, 3));
+
+        // update ith bit
+        System.out.println(updateIthBit(10, 2, 1));
 
     }
 }
